f24a48e0571526c98aee7a54e01b58a702f78f6e
Puziichuk Olena <nonna467@gmail.com>
2021-04-15 11:21:46 +0300

    added checkbox

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ menuContainer.insertAdjacentHTML("beforeend", menuCards(menu));
   const Switch = document.querySelector("#theme-switch-toggle");
   Switch.addEventListener('change',showSwitch);
 
+  const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
 
   function showSwitch(e){
+    e.preventDefault();
       if (e.target.checked){
-      document.body.classList.add('light-theme');
-      document.body.classList.remove('dark-theme');
-      } else{
       document.body.classList.add('dark-theme');
       document.body.classList.remove('light-theme');
+      localStorage.setItem('theme', Theme.DARK);
+      } else{
+      document.body.classList.add('light-theme');
+      document.body.classList.remove('dark-theme');
+      localStorage.setItem('theme', Theme.LIGHT);
+      
   }
   };
